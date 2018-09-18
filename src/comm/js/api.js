8af755a7abf04b5442b588c40b7f0e517a174757
yijian002@gmail.com <yijian002@gmail.com>
2018-09-18 20:04:41 +0800

    npm axios

---
+++ src/comm/js/api.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const BASE = '';
+
+export default {
+    get(url, params) {
+        return axios.get(BASE + url, { params: params || {} }).then(res => res.data)
+            .catch(function(error) {
+                return { code: 500, message: error.message };
+            });
+    },
+    post(url, params) {
+        return axios.post(BASE + url, params || {}).then(res => res.data)
+            .catch(function(error) {
+                return { code: 500, message: error.message };
+            });
+    },
+    put(url, params) {
+        return axios.put(BASE + url, params || {}).then(res => res.data)
+            .catch(function(error) {
+                return { code: 500, message: error.message };
+            });
+    },
+    del(url, params) {
+        return axios.delete(BASE + url, { params: params || {} }).then(res => res.data)
+            .catch(function(error) {
+                return { code: 500, message: error.message };
+            });
+    },
+    delete(url, params) {
+        return this.del(url, params);
+    },
+    gourl(url, params) {
+        let opts = [];
+        for (let key in params) {
+            opts.push(key + '=' + params[key]);
+        }
+        window.location.href = BASE + url + '?' + opts.join('&');
+    }
+};
